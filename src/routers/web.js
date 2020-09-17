b153db484a78c579ae61aeefeafb70fc97bce1ea
Hoàn Phạm Lê <hoanvn1999@gmail.com>
2020-09-17 11:29:19 +0700

    Da sua router, chay duoc

--- src/routers/web.js
+++ src/routers/web.js
@@ -2,12 +2,16 @@ import express from 'express'
 
 import HomeController  from "../controllers/home.controller";
 
+import productRoute from "./product";
+import accountRoute from "./account";
+
 let router = express.Router();
 
 let webRouter = (app) => {
-    app.use("/", router);
-    //index route
-    router.get("/", HomeController.homepage);
+    //product route
+    app.use("/product", productRoute);
+    //account route
+    app.use("/", accountRoute);
 }
 
 module.exports = webRouter;
