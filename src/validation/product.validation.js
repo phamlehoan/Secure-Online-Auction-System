8d12832553cf76cc85e038c8346c788c0a9c0d38
Boeing <dacvinh98@gmail.com>
2020-10-18 14:22:27 +0700

    Import missing pakage

--- src/validation/product.validation.js
+++ src/validation/product.validation.js
@@ -1,20 +1,21 @@
+import {check} from 'express-validator';
 import moment from 'moment';
 import { productError } from './../langs/us/product.noti.us';
 
 const ProductValid = {}
-
+const dayFormat = "YYYY-MM-DD LT";
 ProductValid.checkAdd = [
     // Check if start date is valid
-    body("aucStartTime", productError.startDate)
+    check("aucStartTime", productError.startDate)
         .exists()
         .custom((value) => {
-            return moment(value, "YYYY-MM-DD LT", true).isValid()
+            return moment(value, dayFormat, true).isValid()
         }),
     // Check if end day is valid
-    body("aucEndTime", productError.endDate)
+    check("aucEndTime", productError.endDate)
         .exists()
         .custom((value) => {
-            return moment(value, "YYYY-MM-DD LT", true).isValid()
+            return moment(value, dayFormat, true).isValid()
         })
 ];
 
